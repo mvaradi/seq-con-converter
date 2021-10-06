1765f9acc10eb09559d4edd107a42e37bf049669
Mihaly Varadi <mvaradi@ebi.ac.uk>
2021-10-06 15:00:36 +0100

    Adding support for getting data via the API

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import sys
+import requests
+import json
+import logging
 from seq_con_json_converter.converter import Converter
 
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+API_URL = "https://www.ebi.ac.uk/pdbe/graph-api/uniprot/sequence_conservation/"
+ACCESSION = sys.argv[1]
 
-# TODO - Get the JSON data from the API instead of manually downloaded JSON files
-# TODO - Get the JSON paths from sys.argv instead of having it hard-coded
-# TODO - Add logging
-# TODO - Add license
-# TODO - Push to a repository
-
-# import requests
-# API_URL = "https://www.ebi.ac.uk/pdbe/graph-api/uniprot/sequence_conservation/"
-# ACCESSION = "P0DTD1"
-# response = requests.get(API_URL + ACCESSION)
+logging.info("Fetching JSON for " + ACCESSION)
+response = requests.get(url=API_URL + ACCESSION).json()
+with open("data/" + ACCESSION + "-old.json", "w") as old_json:
+    logging.info("Saving old JSON for " + ACCESSION)
+    json.dump(response, old_json)
 
 
 if __name__ == '__main__':
-    converter = Converter("./data/old.json", "./data/new.json")
+    converter = Converter("data/" + ACCESSION + "-old.json", "data/" + ACCESSION + "-new.json")
     converter.load_json()
+    logging.info("Converting JSON for " + ACCESSION)
     converter.convert()
     converter.save_json()
+    logging.info("Saving new JSON for " + ACCESSION)
